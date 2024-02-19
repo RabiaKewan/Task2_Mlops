73b74fb8a8882c8cd6b426028053fa4150bc14ab
RabiaKewan <119858275+RabiaKewan@users.noreply.github.com>
2024-02-19 11:01:52 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,18 @@
-import unittest
-
+import pytest
 from main import StudentsInMLOps
 
-class TestStudentsInMLOps(unittest.TestCase):
+@pytest.fixture
+def students():
+    return StudentsInMLOps()
 
-    def test_enroll_students(self):
-        mlops_class = StudentsInMLOps()
-        mlops_class.enrollStudents(5)
-        self.assertEqual(mlops_class.getTotalStrength(), 5)
+def test_enroll_students(students):
+    students.enrollStudents(5)
+    assert students.getTotalStrength() == 5
 
-    def test_drop_students(self):
-        mlops_class = StudentsInMLOps()
-        mlops_class.enrollStudents(10)
-        mlops_class.dropStudents(3)
-        self.assertEqual(mlops_class.getTotalStrength(), 7)
+def test_drop_students(students):
+    students.enrollStudents(10)
+    students.dropStudents(5)
+    assert students.getTotalStrength() == 5
 
-    def test_get_total_strength(self):
-        mlops_class = StudentsInMLOps()
-        mlops_class.enrollStudents(20)
-        self.assertEqual(mlops_class.getTotalStrength(), 20)
-
-    def test_get_class_name(self):
-        mlops_class = StudentsInMLOps()
-        self.assertEqual(mlops_class.getClassName(), "MLOps")
-
-if __name__ == '__main__':
-    unittest.main()
+def test_get_class_name(students):
+    assert students.getClassName() == 'MLOps'
